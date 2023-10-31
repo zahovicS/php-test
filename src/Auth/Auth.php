415625109f109f6bf9a1ee171c46222b181cf9cb
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2023-10-31 11:31:43 -0500

    Update Auth.php

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -6,7 +6,7 @@ use Src\Database\DB;
 
 class Auth
 {
-    public function attempt($email, $password)
+    public function attempt(string $email,string $password): bool
     {
         $user = DB::query('SELECT * FROM usuarios WHERE email = :email', [
             'email' => $email
@@ -23,13 +23,23 @@ class Auth
         return false;
     }
 
-    public function login($user)
+    public function login(array $user): void
     {
         $_SESSION['user'] = [
             'email' => $user['email']
         ];
 
         session_regenerate_id(true);
+    }
+    
+    public function user():?array
+    {
+        return isset($_SESSION['user']) && !empty($_SESSION['user']) ?? null;
+    }
+
+    public function check(): bool
+    {
+        return isset($_SESSION['user']);
     }
 
     public function logout()
